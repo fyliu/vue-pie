2313cad9ec0398e6a28bcf397002f03da9c7000a
Fang Liu <fangyiliu@gmail.com>
2018-07-26 13:12:44 -0700

    data as array
    2 way binding doesn't work for values in v-for loops

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,34 @@
   <div class="hello">
     <pie-chart :data="chartDataPie"></pie-chart>
     <!--<line-chart :data="chartData"></line-chart>-->
+    <hr>
+    <div>
+      Chart Name: <input type="text" v-model="chartName">
+    </div>
     <ul>
       <li v-for="d in chartDataPie">
         {{d[0]}} {{d[1]}}
       </li>
     </ul>
-    <input type="text" v-model="chartDataPie[0][0]">
+    <div v-for="d in chartDataPie">
+      <input type="text" v-model="d[0]">
+      <input type="text" v-model="d[1]">
+    </div>
+    <div v-for="d in chartDataPie">
+      Input Value <span v-text="d[0]"></span>
+      Input Value <span v-text="d[1]"></span>
+    </div>
+    <!--
+    <div>
+      <input type="text" v-model="d[1]">
+    </div>
+    <div>
+      <input type="text" v-model="d[1]">
+    </div>
+    <div>
+      <input type="text" v-model="d[1]">
+    </div>
+    -->
   </div>
 </template>
 
@@ -18,8 +40,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      chartName: '',
       chartData: [["Jan", 4], ["Feb", 2], ["Mar", 10], ["Apr", 5], ["May", 3]],
       chartDataPie: [['Blueberry', 44], ['Strawberry', 23]]
+      //chartDataPie: {'Blueberry': 44, 'Strawberry': 23}
     }
   }
 }
